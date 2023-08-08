42ac38ff71db4add731a7d0d6e1d88424722d0a0
rliechti <rliechti@adobe.com>
2023-08-08 11:33:09 +0200

    yolo

--- blocks/franklin-banner/franklin-banner.js
+++
@@ -1,6 +0,0 @@
-import { fetchPlaceholders, getMetadata } from '../../scripts/lib-franklin.js';
-
-export default async function decorate(block) {
-    const placeholders = await fetchPlaceholders('');
-    alert(placeholders);
-}
